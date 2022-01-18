2b4b495317baa5df4ab3279769e2af1b32a4ea39
Ovi Sofariu <ovi@sofariu.com>
2022-01-18 17:20:34 -0500

    switch to Chrome for tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,32 +1,36 @@
 const webpackConfig = require('./webpack.test');
 
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    plugins: [
-      require('karma-chrome-launcher'),
-      require('karma-spec-reporter'),
-      require('karma-jasmine'),
-      require('karma-webpack'),
-    ],
-    frameworks: ['jasmine'],
-    files: [
-      'src/test.js'
-    ],
-    exclude: [],
-    preprocessors: {
-      'src/test.js': [ 'webpack']
-    },
-    reporters: ['spec'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    concurrency: Infinity,
-    webpack: {
-      ...webpackConfig
-    }
-  });
+    config.set({
+        basePath: '',
+        plugins: [
+            require('karma-chrome-launcher'),
+            require('karma-spec-reporter'),
+            require('karma-jasmine'),
+            require('karma-webpack'),
+            require('karma-jasmine-html-reporter')
+        ],
+        frameworks: ['jasmine'],
+        files: [
+            'src/test.js'
+        ],
+        exclude: [],
+        preprocessors: {
+            'src/test.js': ['webpack']
+        },
+        client: {
+            clearContext: false
+        },
+        reporters: ['kjhtml', 'spec'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['Chrome'],
+        singleRun: false,
+        concurrency: Infinity,
+        webpack: {
+            ...webpackConfig
+        }
+    });
 };
